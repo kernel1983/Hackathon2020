57c477f966e9ff0766aa6b1dc8d4cd3c407d3954
KJ <kernel1983@gmail.com>
2019-08-01 20:46:00 +0800

    new tx script

---
+++ new_tx.py
@@ -0,0 +1,94 @@
+from __future__ import print_function
+
+import os
+import subprocess
+import time
+import socket
+import argparse
+import random
+import uuid
+import base64
+import hashlib
+import urllib
+
+import tornado.web
+import tornado.websocket
+import tornado.ioloop
+import tornado.options
+import tornado.httpserver
+import tornado.gen
+import tornado.escape
+
+from ecdsa import SigningKey, NIST256p
+
+
+USER_NO = 10
+count = 10
+users = {}
+transactions = []
+
+
+def tx():
+    # known_addresses_list = list(ControlHandler.known_addresses)
+    known_addresses_list = [("192.168.1.4", "8002")]
+    addr = random.choice(known_addresses_list)
+
+    print(len(transactions), addr)
+    http_client = tornado.httpclient.AsyncHTTPClient()
+    # try:
+    #     response = yield http_client.fetch("http://%s:%s/new_tx" % tuple(addr), method="POST", body=tornado.escape.json_encode(data))
+    # except Exception as e:
+    #     print("Error: %s" % e)
+    data = transactions.pop()
+    http_client.fetch("http://%s:%s/new_tx" % tuple(addr), method="POST", body=tornado.escape.json_encode(data), callback=cb)
+
+def cb(response):
+    if len(transactions) == 0:
+        tornado.ioloop.IOLoop.instance().stop()
+        return
+    tornado.ioloop.IOLoop.instance().add_callback(tx)
+
+def main():
+    for n in range(USER_NO):
+        user_filename = "pk" + str(n)
+        user_sk = SigningKey.from_pem(open("data/pk/"+user_filename).read())
+        users[n] = user_sk
+        print("load key", n)
+
+    for n in range(0, count*2, 2):
+        # user_nos = set(range(USER_NO))
+        # i = random.choice(list(user_nos))
+        sender_sk = users[n%USER_NO]
+        sender_vk = sender_sk.get_verifying_key()
+        sender = base64.b64encode(sender_vk.to_string()).decode()
+
+        # j = random.choice(list(user_nos - set([i])))
+        receiver_sk = users[n%USER_NO+1]
+        receiver_vk = receiver_sk.get_verifying_key()
+        receiver = base64.b64encode(receiver_vk.to_string()).decode()
+
+        amount = random.randint(1, 20)
+        txid = uuid.uuid4().hex
+        timestamp = int(time.time())
+        transaction = {
+            "txid": txid,
+            "sender": sender,
+            "receiver": receiver,
+            "timestamp": timestamp,
+            "amount": amount
+        }
+        # sender_sign = signing.Signer(sender_sk)
+        signature = sender_sk.sign(str(timestamp).encode("utf8"))
+        data = {
+            "transaction": transaction,
+            "signature": base64.b64encode(signature).decode()
+        }
+
+        # assert sender_vk.verify(signature, str(timestamp).encode("utf8"))
+        print("gen tx", n)
+        transactions.append(data)
+
+    tx()
+
+tornado.ioloop.IOLoop.instance().add_callback(main)
+tornado.ioloop.IOLoop.instance().start()
