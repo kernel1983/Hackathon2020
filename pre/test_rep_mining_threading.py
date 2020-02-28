1da8f9a16ed4c3be55761480aeed7b4ab64ed1d3
KJ <kernel1983@gmail.com>
2020-02-28 22:30:32 +0800

    threading rep

---
+++ pre/test_rep_mining_threading.py
@@ -0,0 +1,75 @@
+from __future__ import print_function
+
+import time
+import random
+import base64
+import threading
+from hashlib import blake2b
+from hashlib import sha1
+from hashlib import sha256
+
+import numbertheory
+import util
+
+from keys import PrivateKey, PublicKey
+
+lock = threading.Lock()
+sk = PrivateKey.generate()
+pk_bytes = sk.verifying_key.to_string()
+
+# l160 = 2**160-1
+# l192 = 2**192-1
+l256 = 2**256-1
+# l512 = 2**512-1
+
+print(pk_bytes)
+t0 = time.time()
+m = l256
+nonce = 0
+position = 1
+
+out = open('./rep.txt', 'w')
+out.write(base64.b16encode(pk_bytes).decode('utf8'))
+
+messages = []
+for i in range(2**13):
+    r = random.randrange(255)
+    messages.append(r)
+
+r = messages.pop(0)
+def thread(n):
+    global position
+    global nonce
+    global m
+    global r
+    # position2 = position
+    while True:
+        d = sha256(pk_bytes)
+        lock.acquire()
+        nonce += 1
+        nonce2 = nonce
+        lock.release()
+
+        d.update(util.number_to_string(position, 2**64-1))
+        d.update(util.number_to_string(nonce2, 2**64-1))
+        if util.string_to_number(d.digest()) < m:
+            m = util.string_to_number(d.digest())
+            print(n, d.hexdigest(), nonce2, position, nonce2/position)
+
+        if d.digest()[-1] == r:
+            out.write("\n"+str(position)+' '+str(nonce2)+' '+str(n)+' '+str(r))
+            if not messages:
+                break
+            lock.acquire()
+            position += 1
+            r = messages.pop(0)
+            lock.release()
+
+ts = []
+for i in range(4):
+    t=threading.Thread(target=thread, args=(i,))
+    ts.append(t)
+for t in ts:
+    t.start()
+for t in ts:
+    t.join()
