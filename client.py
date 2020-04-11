4e8575020e6665cb8e33bd41be3969324e8903c7
KJ <kernel1983@gmail.com>
2020-04-12 00:48:17 +0800

    a cmd line client

---
+++ client.py
@@ -0,0 +1,18 @@
+
+import base64
+from ecdsa import SigningKey, NIST256p
+
+# for i in range(10):
+#     sk_filename = "pk%s" % i
+#     # sk = keys.UmbralPrivateKey.gen_key()
+#     # open("data/pk/"+sk_filename, "w").write(sk.to_bytes().hex())
+#     sk = SigningKey.generate(curve=NIST256p)
+#     open("data/pk/"+sk_filename, "w").write(bytes.decode(sk.to_pem()))
+
+# login with sk
+# first file to upload
+# split into blobs
+# query nodes and get response
+# create meta
+# upload meta
+# write on chain
